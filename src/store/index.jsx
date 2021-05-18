0f48d8cbae4989e25383f36b762430249f575759
khoacao0601 <khoacao0601@gmail.com>
2021-05-18 14:26:43 -0700

    Update 05/18/2021 16:25 add Users Store Redux

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,11 +2,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 
 import viewReducer from './reducer/viewsControlSlice';
+import usersReducer from './reducer/usersControlSlice';
 
 //Store
 const store = configureStore({
     reducer: {
-        viewReducer 
+        viewReducer,
+        usersReducer
     }
 })
 
